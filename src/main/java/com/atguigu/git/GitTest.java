3a6043ba68e133afae80be35a879ad0e1e70ca2c
yexue <1453646819@qq.com>
2021-09-09 14:00:15 +0800

    hot_commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("2222");
         System.out.println("2222");
         System.out.println("hot_fix");
+        System.out.println("hot_fix2");
 
     }
 
