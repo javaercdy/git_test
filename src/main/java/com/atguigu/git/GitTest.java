d5badecbee843732e9a2e4a36d842dafc338edfc
yexue <1453646819@qq.com>
2021-09-09 13:46:20 +0800

    second_commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,8 +1,14 @@
 package com.atguigu.git;
 
+
+
 /**
  * @author javaercdy
  * @create 2021-09-09$-{TIME}
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+
 }
